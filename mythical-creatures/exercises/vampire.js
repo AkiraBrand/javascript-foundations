df7c4c3353147cdd6ef648437ab2e1b5956e5c46
Ruth Brand <ruthbrandsoprano@gmail.com>
2021-02-18 10:30:06 -0700

    vampire unicorn dragon

---
+++ mythical-creatures/exercises/vampire.js
@@ -0,0 +1,14 @@
+class Vampire {
+    constructor(name, pet= "bat", thirsty=true){
+  this.name = name;
+  this.pet = pet;
+  this.thirsty = thirsty;
+  }
+
+  drink() {
+    this.thirsty = false;
+  }
+
+}
+
+module.exports = Vampire;
